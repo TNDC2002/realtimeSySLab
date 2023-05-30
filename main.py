a62b0c8667e972fe8bee1346e9361d37cac9b4ff
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-30 10:59:18 +0700

    upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+import scheduler
+from task1 import *
+from task2 import *
+
+scheduler = scheduler.Scheduler()
+scheduler.SCH_Init()
+task1 = Task1()
+task2 = Task2()
+
+
+scheduler.SCH_Add_Task(task1.Task1Run, 1000, 5000)
+scheduler.SCH_Add_Task(task2.Task2Run, 2000, 4000)
+
+while True:
+    scheduler.SCH_Update()
+    scheduler.SCH_Dispatch_Tasks()
+    time.sleep(1)
+
+
+
+
+    # max 40 different tasks
+
+
